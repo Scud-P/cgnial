8cabb0627756dbbc2082c4ba282263cce41c7dbd
PScud <escudie.pierre@gmail.com>
2024-04-26 07:10:02 -0400

    Initialization

---
+++ src/main/java/com/cgnial/salesreports/SalesreportsApplication.java
@@ -0,0 +1,13 @@
+package com.cgnial.salesreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesreportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesreportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cgnial/salesreports/SalesreportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgnial.salesreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesreportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
